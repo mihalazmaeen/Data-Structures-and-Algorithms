6133eb53cae3974b1cdf7e013e02c1822e22bf5b
mihalazmaeen <azmaeenmihal1@gmail.com>
2022-12-05 14:09:14 +0600

    insertion sort

---
+++ insertionsort.js
@@ -0,0 +1,15 @@
+function insertionsort(arr) {
+    for (let i = 1; i < arr.length ; i++){
+        let numberToInsert = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > numberToInsert) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+
+        }
+        arr[j + 1] = numberToInsert;
+    }
+}
+const arr = [8, 20, 4, -6, -8];
+insertionsort(arr);
+console.log(arr);
