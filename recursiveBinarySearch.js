3c4efa3b4f11a9423ce306e2b0fd7036a1df6c0a
mihalazmaeen <azmaeenmihal1@gmail.com>
2022-12-05 13:05:58 +0600

    recursive binary search

---
+++ recursiveBinarySearch.js
@@ -0,0 +1,18 @@
+function recursiveBinarySearch(arr,target) {
+    return search(arr,target,0,arr.length-1)
+}
+function search(arr, target, leftIndex, rightIndex) {
+    if (leftIndex > rightIndex) {
+        return -1;
+    } let middleIndex = Math.floor((leftIndex + rightIndex) / 2)
+    if (target === arr[middleIndex]) {
+        return middleIndex
+    }
+    if (target < arr[middleIndex]) {
+       return search(arr, target, leftIndex, middleIndex - 1);
+    } else {
+       return search(arr, target, leftIndex + 1, rightIndex);
+    }
+}
+console.log(recursiveBinarySearch([-5, 4, 6, 3, 5, 10], 6));
+// big O=O(logn)
