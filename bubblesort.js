83b954edbbe00ce54204caddbcf27ef91a17c8d1
mihalazmaeen <azmaeenmihal1@gmail.com>
2022-12-05 13:22:06 +0600

    bubble sort

---
+++ bubblesort.js
@@ -0,0 +1,19 @@
+function bubblesort(arr) {
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    }while(swapped)
+}
+const arr = [8, 20, 4, -6, -8];
+bubblesort(arr);
+console.log(arr);
+
+// O(n^2)
